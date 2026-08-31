02d39b98001bd87ced61729b21dc8a121fb1ab8b
Raheel <raheel2k29@gmail.com>
2026-09-01 00:01:17 +0500

    Add logic to parse Google JSON key from env var

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+﻿import os
 import time
 import schedule
 import subprocess
+
+# Write the Google JSON Key from the Environment Variable to a temporary file
+if 'GOOGLE_JSON_KEY' in os.environ:
+    creds_path = '/tmp/google_creds.json'
+    # Fallback for Windows local testing
+    if os.name == 'nt':
+        creds_path = 'google_creds.json'
+        
+    with open(creds_path, 'w') as f:
+        f.write(os.environ['GOOGLE_JSON_KEY'])
+    
+    # Tell the Google Cloud library where to find the key file
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = creds_path
+    print("Successfully loaded Google Cloud credentials.")
 
 def run_shopify():
     print("Running Shopify Pipeline...")
